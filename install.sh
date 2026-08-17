aacfed89d800432b8c06541fed9fd68b86fc32fc
r1story <97712077+r1story@users.noreply.github.com>
2026-08-17 21:07:41 +0200

    Add installation script for system configuration
    v1.0

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#============
+#
+#         SCRIPT DE CONFIGURATION
+#
+#============
+
+echo "Début du script de configuration"
+sleep 2
+
+#============
+
+echo "Phase 1 : Mise à jour du système"
+sleep 2
+
+apt update && apt upgrade -y
+apt autoremove -y
+
+#============
+
+echo "Phase 2 : Installation des utilitaires de base"
+sleep 2
+
+apt install sudo curl wget nano tree zip unzip openssh-client openssh-server htop dnsutils
+
+#============
+
+echo "Phase 3 : Création de l'utilisateur Ciel"
+sleep 2
+
+useradd -m -G sudo Ciel
+echo "Définir le mot de passe de l'utilisateur Ciel :"
+psswd Ciel 
+
+#============
+
+echo "Phase 4 : Définition de quelques alias"
+sleep 2
+
+alias ll='ls -lah'
+alias la='ls -A'
+alia grep='grep --color-auto'
